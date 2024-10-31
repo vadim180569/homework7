b8a209b1966b9631d89fc2cddc9f16f842137f44
vadim180569 <vadim9771505001@gmail.com>
2024-10-31 16:38:51 +0300

    Add files via upload

---
+++ pythonProject4/main.py
@@ -0,0 +1,14 @@
+exemple = "Пример"
+print(exemple[0])
+print(exemple[-1])
+print(exemple[3:])
+print(exemple[::-1])
+print(exemple[1::2])
+exemple = "Топинамбур"
+print(exemple[0])
+print(exemple[-1])
+print(exemple[5:])
+print(exemple[::-1])
+print(exemple[1::2])
+
+
